1a047ae7db25d146024e9dc57e2513fa7284daf7
Franchaisco <f.babila.niba@student.vu.nl>
2023-04-11 16:27:03 +0200

    F

--- chat.py
+++ chat.py
@@ -3,7 +3,10 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host_port = ("143.47.184.219", 5378)
 sock.connect(host_port)
-sock.send("HELLO-FROM abbe\n".encode("utf-8"))
+
+username = input("Enter valid Username: ")
+username_mes = "HELLO-FROM " + username + "\n"
+sock.send(username_mes.encode("utf-8"))
 
 while True:
     data = sock.recv(4096).decode("utf-8").strip()
@@ -12,6 +15,7 @@ while True:
 
     if not data:
         break
+
 
 sock.close()
 
