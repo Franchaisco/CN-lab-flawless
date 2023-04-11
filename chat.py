90fd927247b1be0a7ba09afe221ddf43e3a4c50f
Timmermans13 <122202198+Timmermans13@users.noreply.github.com>
2023-04-11 15:59:42 +0200

    sdlkfdfj

--- chat.py
+++ chat.py
@@ -4,11 +4,19 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host_port = ("143.47.184.219", 5378)
 sock.connect(host_port)
 
-string_bytes = "Sockets are great!".encode("utf-8")
-bytes_len = len(string_bytes)
-num_bytes_to_send = bytes_len
-while num_bytes_to_send > 0:
 # Sometimes, the operating system cannot send everything immediately.
 # For example, the sending buffer may be full.
 # send returns the number of bytes that were sent.
-num_bytes_to_send -= sock.send(string_bytes[bytes_len-num_bytes_to_send:])
+# num_bytes_to_send = sock.send(string_bytes[bytes_len-num_bytes_to_send:])
+
+sock.send("HELLO-FROM abbe\n".encode("utf-8"))
+
+while True:
+    data = sock.recv(4096).decode("utf-8").strip()
+
+    print(data)
+
+    if not data:
+        break
+
+sock.close()
