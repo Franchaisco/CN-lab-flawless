c69fb8337804f26580d61763a96d23820dae8484
Timmermans13 <122202198+Timmermans13@users.noreply.github.com>
2023-04-11 16:31:07 +0200

    sd

--- chat.py
+++ chat.py
@@ -3,19 +3,18 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host_port = ("143.47.184.219", 5378)
 sock.connect(host_port)
-
-username = input("Enter valid Username: ")
-username_mes = "HELLO-FROM " + username + "\n"
-sock.send(username_mes.encode("utf-8"))
+username = input("Enter valid username:")
+message = 
+sock.send("HELLO-FROM \n".encode("utf-8"))
 
 while True:
     data = sock.recv(4096).decode("utf-8").strip()
 
     print(data)
+    sock.close()
 
     if not data:
         break
-
 
 sock.close()
 
