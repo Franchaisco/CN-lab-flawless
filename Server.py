f6f643a1d44e36053073ef6a80a5859bbeab7d43
Franchaisco <f.babila.niba@student.vu.nl>
2023-05-11 19:45:52 +0200

    d

--- Server.py
+++ Server.py
@@ -4,11 +4,12 @@ import threading
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 while True:
     host_port = ("127.0.0.1", 4321)
-    sock.connect(host_port)
+    sock.bind(host_port)
     sock.listen(3000)
     data = sock.recv(4096).decode("utf-8").strip()
-    hello_msg = "HELLO-FROM"
-    hello = hello_msg + " "
-    if data.startswith(hello_msg)
-        username = hello[10:]
-        sock.send("HELLO " + username).encode("utf-8")
+    
+try:
+    sock.send("how to handle errors?".encode("utf-8"))
+    answer = sock.recv(4096)
+except OSError as msg:
+    print(msg)
