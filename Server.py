b5316990c964e5fad39e7f24d3ec1923c84846c2
Franchaisco <f.babila.niba@student.vu.nl>
2023-05-13 18:59:11 +0200

    bk

--- Server.py
+++ Server.py
@@ -1,15 +1,40 @@
 import socket
 import threading
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-while True:
-    host_port = ("127.0.0.1", 4321)
-    sock.bind(host_port)
-    sock.listen(3000)
-    data = sock.recv(4096).decode("utf-8").strip()
+host = socket.gethostbyname(socket.gethostname())
+port = 5353
+address = (host, port)
+
+def receive_client(conn, addr):
+    print("New connection! " + str(addr) + " connected")
     
-try:
-    sock.send("how to handle errors?".encode("utf-8"))
-    answer = sock.recv(4096)
-except OSError as msg:
-    print(msg)
+    connection = True
+    while connection:
+        mes = conn.recv(4096).decode("utf-8").strip()
+        if mes == "!quit":
+            mes = "You've disconnected from the server"
+            sock.send(mes.encode("utf-8"))
+            connection = False
+        elif mes.startswith("HELLO_FROM"):
+            mes = "HELLO " + str(addr)
+            sock.send(mes.encode("utf-8"))
+        elif mes.startswith("LIST"):
+            
+        elif mes.startswith("SEND"):
+            print("SEND-OK")
+        conn.close()
+
+
+
+
+
+print("Initiate server...")
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(address)
+sock.listen()
+print("Listening on " + host + ": " + str(port))
+
+while True:
+    conn, addr = sock.accept()
+    thr = threading.Thread(target=receive_client, args=(conn, addr))
+    thr.start()
